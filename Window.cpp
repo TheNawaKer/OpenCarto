80a6739d135a122292f257f9965839129492cf48
stoyan <stoyanserin@gmail.com>
2014-04-02 14:15:48 +0200

    ajout de Window

---
+++ Window.cpp
@@ -0,0 +1,20 @@
+#include "Window.hpp"
+
+void Window::reshape(int w,int h){
+	glViewport (0, 0, (
+}
+
+void Window::display(void){
+
+}
+
+void Window::framerate(void){
+
+}
+
+void Window::key(unsigned char key , int x , int y ){
+
+
+}
+
+
---
+++ Window.hpp~
@@ -0,0 +1,15 @@
+#ifndef WINDOW
+#define WINDOW
+
+class Window {
+	private:
+		
+	public:
+	Window(){}
+	void reshape(int , int);
+	void display(void);
+	void framerate(void);
+	void key(unsigned char,int,int);
+};
+
+#endif
